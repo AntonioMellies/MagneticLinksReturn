ac9281b0bb4787ebf258305a005860f36bb5c3a7
AntonioMellies <neto-fred@live.com>
2019-05-13 22:43:16 -0300

    Config_5

--- MagneticLinksReturn/settings.py
+++ MagneticLinksReturn/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*=lbv#r6oy^5fdyu5p%hpi3un263jr+)yr$2$f@0$okcp+%fa5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
